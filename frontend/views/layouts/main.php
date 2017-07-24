956dc5aaeba85ead1c411d8b60d38aec95edeb7f
Vladimir Alekseev <a.vovka@gmail.com>
2017-07-24 12:14:39 +0300

    products

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -5,6 +5,7 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\bootstrap\Modal;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
@@ -72,7 +73,60 @@ $menuItems = [
                     <li>Тонизирует и омолаживает</li>
 				</ul>
 				<h2 class="header-ttl">Здоровое похудение в каждой ложке</h2>
+				<ul class="price">
+                    <li>
+                      <span class="top">Скидка</span>
+                      <span><small>до</small> 67%</span> 
+                      <span class="bottom">сегодня</span>
+                    </li>
+                    <li>
+                      <span class="top">Цена</span>
+                      <span>290 грн</span>
+                      <span class="old bottom j-s_old_price_curs">580 грн</span>
+                    </li>
+				</ul>
+				<div class="text-center">
+					<a class="btn btn-red btn-big" href="#order">Заказать сейчас</a>
+				</div>
+				<div class="guaranty" id="guaranty-open"><a class="add-quote" data-toggle="modal" data-target="#guaranty" href="#guaranty"><img src="/i/guaranty-new.png" /></a></div>
+				<?php Modal::begin([
+                    'header' => '<h3>'.\Yii::t('app', 'Гарантия возврата денег').'</h3>',
+                    'id' => 'guaranty',
+                    'options' =>['class'=>'modal-guaranty'],
+                ]);?>
+                Eсли Вы приобрели препарат, но передумали его использовать Вы можете вернуть его в течении 1 месяца и получить обратно его полную стоимость, за исключением затрат на пересылку. При этом товар не должен быть вскрыт или использован. Если Вы возвращаете второй или третий флакон, который покупали по скидочной цене, тогда возвращается скидочная стоимость флакона, за исключением затрат на пересылку.
+				<?php Modal::end();?>
 			</div>
+			
+			<div class="bottls">
+            	<div class="it it-1">
+            		<div class="img"><img src="/i/b-1-3.png"></div>
+            		<div class="price-old">580 <span>грн</span></div>
+            		<div class="price-new">290 <span>грн / 1 шт</span></div>
+    				<a class="btn btn-red btn-big" href="#order" data-count="1">Заказать</a>
+            	</div>
+            	<div class="it it-2">
+            		<div class="img"><img src="/i/b-2-3.png"></div>
+            		<div class="price-old">1160 <span>грн</span></div>
+            		<div class="price-new">450 <span>грн / 2 шт</span></div>
+    				<a class="btn btn-red btn-big" href="#order" data-count="2">Заказать</a>
+            	</div>
+            	<div class="it it-3">
+            		<div class="img"><img src="/i/b-3-3.png"></div>
+            		<div class="price-old">1740 <span>грн</span></div>
+            		<div class="price-new">590 <span>грн / 3 шт</span></div>
+    				<a class="btn btn-red btn-big" href="#order" data-count="3">Заказать</a>
+            	</div>
+            </div>
+            
+            <div class="star-text-2">* Результат зависит от индивидуального восприятия и может отличаться.<br>Сироп не является лекарственным средством.</div>
+			
+			<div class="price-new-10-block">
+	        	<div class="price-new-10">1500 <span>грн / 10 шт</span><br/>
+	        		<a class="btn btn-red btn-big" href="#" id="price10">Подробнее</a>
+	        	</div>
+	        </div>
+			
 		</div>
     </div>
     <div id="holder"></div>
